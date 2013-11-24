8985e5eb0fd0b4157cfad9aae4c740178a42b98f
Habib Alamin <ha.alamin@gmail.com>
2013-11-24 12:16:51 +0000

    Added ArtworkResult class

---
+++ GUI/dArt/ArtworkResult.h
@@ -0,0 +1,14 @@
+//
+//  ArtworkResult.h
+//  dArt
+//
+//  Created by Habib A on 24/11/2013.
+//  Copyright (c) 2013 alAmin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ArtworkResult : NSObject
+@property NSString *title;
+@property NSImage *image;
+@end
---
+++ GUI/dArt/ArtworkResult.m
@@ -0,0 +1,14 @@
+//
+//  ArtworkResult.m
+//  dArt
+//
+//  Created by Habib A on 24/11/2013.
+//  Copyright (c) 2013 alAmin. All rights reserved.
+//
+
+#import "ArtworkResult.h"
+
+@implementation ArtworkResult
+@synthesize title;
+@synthesize image;
+@end
